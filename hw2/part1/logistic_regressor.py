filled in the loss and gradient for the correct class

hw2/part1/logistic_regressor.py
@@ -99,7 +99,7 @@ class LogisticRegressor:
         # regression                                                             #
         # TODO: 1 line of code expected                                          #
         ##########################################################################
-        
+        grad = np.dot(X.T, sigmoid(np.dot(X,theta)) - y) / m
 
         ###########################################################################
         #                           END OF YOUR CODE                              #
@@ -209,7 +209,8 @@ class RegLogisticRegressor:
         # Compute the loss function for regularized logistic regression          #
         # TODO: 1-2 lines of code expected                                       #
         ##########################################################################
-
+        h = sigmoid(np.dot(X,theta))
+        J = np.mean(-y*np.log(h) - (1-y)*np.log(1-h)) + reg*(np.dot(theta, theta) - theta[0]**2) / (2*m)
 
         ###########################################################################
         #                           END OF YOUR CODE                              #
@@ -238,7 +239,7 @@ class RegLogisticRegressor:
         # regression                                                             #
         # TODO: 1 line of code expected                                          #
         ##########################################################################
-
+        grad = (np.dot(X.T, sigmoid(np.dot(X,theta)) - y) + reg*theta*np.vstack([0, np.ones([dim-1, 1])])) / m
 
         ###########################################################################
         #                           END OF YOUR CODE                              #