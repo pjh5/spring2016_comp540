2e349d43a215d0c7e10f7b41151151832cfc628a
Jesse <pjh5@rice.edu>
2016-02-02 13:25:46 -0600

    add part 2 except for music folder

---
+++ hw2/part2/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+######################################################################################
+#   The sigmoid function                                                             #
+#     Input: z: can be a scalar, vector or a matrix                                  #
+#     Output: sigz: sigmoid of scalar, vector or a matrix                            #
+#     TODO: 1 line of code expected                                                  #
+######################################################################################
+
+def sigmoid (z):
+    sig = np.zeros(z.shape)
+    # Your code here
+    # 1 line expected
+
+    # End your ode
+
+    return sig
